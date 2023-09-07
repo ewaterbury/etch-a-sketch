bae74390acfa76e74bd4cbec180948850c437364
potetpatata <ericwaterbury70@gmail.com>
2023-09-07 16:15:52 -0700

    Added draw function to eetch-a-sketch, updated styling, and added a variable grid size feature.

--- eas.js
+++ eas.js
@@ -1,22 +1,43 @@
-
-
-function generateEtchGrid() {
+function generateGrid(gridSize) {
     let rowNum = 0;
-    for(let i = 0; i < 16; i++){
+    for(let i = 0; i < gridSize; i++){
         const etchContainer = document.getElementById("etchContainer");
         const etchRow = document.createElement('div');
         rowNum = ++rowNum;
         etchRow.setAttribute('class', 'etchRow');
         etchRow.setAttribute('id', `Row${rowNum}`)
-        //here will be where I style row
         etchContainer.appendChild(etchRow);
         const etchBox = document.createElement('div');
-        //here will be where I style box
         etchBox.textContent = '\u00A0';
         etchBox.setAttribute('class', 'etchBox')
-        for(let i = 0; i < 16; i++){etchRow.appendChild(etchBox.cloneNode(true));}
-}}
+        etchBox.style.backgroundColor = 'white';
+        for(let i = 0; i < gridSize; i++){etchRow.appendChild(etchBox.cloneNode(true));}
+    }
+    const gridBoxes = document.querySelectorAll("div.etchBox");
+    gridBoxes.forEach((gridBox) => {
+        gridBox.addEventListener('mouseenter', () => {gridBox.style.backgroundColor = 'black';})
+    });
+}
+
+generateGrid(16);
+
+function newGrid() {
+    let input = document.getElementById("gridSizeInput").value;
+    if(input > 64 || input < 4 || isNaN(input)) {
+        alert("Don't be a dumbass!");
+        return;
+    }
+    let deleteRows = document.querySelectorAll("div.etchRow");
+    deleteRows.forEach((deleteRow) => {deleteRow.remove();}) 
+    generateGrid(input);
+    const gridBoxes = document.querySelectorAll("div.etchBox");
+    gridBoxes.forEach((gridBox) => {
+        gridBox.addEventListener('mouseenter', () => {gridBox.style.backgroundColor = 'black';})
+    });
+}
+
+const gridButton = document.getElementById("gridSizeButton");
+gridButton.addEventListener("click", newGrid);
 
 
 
-generateEtchGrid();
